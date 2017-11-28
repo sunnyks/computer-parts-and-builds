53998975aa74f41e47339fba16f199d1219edd14
Sunny <sunnyksharma1@gmail.com>
2017-11-27 22:43:41 -0500

    Added login and part creation/editing

--- application.py
+++ application.py
@@ -26,19 +26,128 @@ session = DBSession()
 
 @app.route('/login')
 def showLogin():
-
-
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(64))
+    login_session['state'] = state
+    return render_template('login.html', STATE = state)
 
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
 
+    # Check that access token in credentials object is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + str(access_token))
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # abort in case of error in accessing of token info
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    # verify that the access token is used for intended user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps("token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    # verify that access token is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-type'] = 'application/json'
+        return response
+    # check to see if user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'), 200)
+        response.headers['Content-type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use
+    #login_session['provider'] = 'google'
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "http://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token' : credentials.access_token, 'alt' : 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = '<h1>hey sup</h1>'
+    return output
+
+
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email = login_session['email'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user.id
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
 
 
 
 @app.route('/gdisconnect')
 def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    print 'in gdisconnect access token is' + str(access_token)
+    print 'username is: '
+    print login_session['username']
+    url = 'https://www.accounts.google.com/o/oauth2/revoke?token=' + str(login_session['access_token'])
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
 
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token'), 400)
+        response.headers['Content-type'] = 'application/json'
+        return response
 
 
 @app.route('/')
@@ -50,25 +159,62 @@ def showHome():
 
 @app.route('/part/')
 def showAllParts():
-
+    parts = session.query(Part).order_by(asc(Part.name))
+    if 'username' in login_session:
+        return render_template('allparts.html', parts = parts)
+    else:
+        return render_template('publicallparts.html', parts = parts)
 
 
 
 @app.route('/part/<int:part_id>/')
 def showPart(part_id):
-
+    part = session.query(Part).filter_by(id = part_id).one()
+    if 'username' in login_session:
+        return render_template('partinfo.html', part_name = part.name, part_type = part.type, part_price = part.price, part_manufacturer = part.manufacturer, part_model_number = part.model_number)
+    else:
+        return render_template('publicpartinfo.html', part_name = part.name, part_type = part.type, part_price = part.price, part_manufacturer = part.manufacturer, part_model_number = part.model_number)
 
 
 
 @app.route('/part/new/', methods = ['GET', 'POST'])
-def createPart():
-
+def newPart():
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        newPart = Part(type = request.form['type'], name = request.form['name'], price = request.form['price'], manufacturer = request.form['manufacturer'], model_number = request.form['model_number'])
+        session.add(newPart)
+        session.commit()
+        flash('New item' + newPart.name + 'successfully added')
+        return redirect(url_for('showPart', part_id = newPart.id))
+    else:
+        return render_template('newPart.html')
 
 
 
 
 @app.route('/part/<int:part_id>/edit', methods = ['GET', 'POST'])
 def editPart(part_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    editedPart = session.query(Part).filter_by(id = part_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedPart.name = request.form['name']
+        if request.form['type']:
+            editedPart.type = request.form['type']
+        if request.form['price']:
+            editedPart.price = request.form['price']
+        if request.form['manufacturer']:
+            editedPart.manufacturer = request.form['manufacturer']
+        if request.form['model_number']:
+            editedPart.model_number = request.form['model_number']
+        session.add(editedPart)
+        session.commit()
+        flash('Part successfully edited')
+        return redirect(url_for('showPart', part_id = part_id))
+    else:
+        return render_template('editPart.html', part_id = part_id)
 
 
 
@@ -87,50 +233,59 @@ def compareParts(part_id1, part_id2):
 
 
 
-@app.route('/part/cpu/')
-def showAllCPUs():
+
+@app.route('/part/<string:part_type>/')
+def showAllPartsOfType():
 
 
 
 
-@app.route('/part/cpucooler/')
-def showAllCPUCoolers():
 
 
-
-
-@app.route('/part/motherboard/')
-def showAllMotherboards():
-
-
-
-
-@app.route('/part/memory/')
-def showAllMemory():
-
-
-
-
-@app.route('/part/storage/')
-def showAllStorage():
-
-
-
-
-@app.route('/part/GPU/')
-def showAllGPUs
-
-
-
-
-@app.route('/part/powersupply/')
-def showAllPowerSupplies():
-
-
-
-
-@app.route('/part/soundcard')
-def showAllSoundcards():
+# @app.route('/part/cpu/')
+# def showAllCPUs():
+#
+#
+#
+#
+# @app.route('/part/cpucooler/')
+# def showAllCPUCoolers():
+#
+#
+#
+#
+# @app.route('/part/motherboard/')
+# def showAllMotherboards():
+#
+#
+#
+#
+# @app.route('/part/memory/')
+# def showAllMemory():
+#
+#
+#
+#
+# @app.route('/part/storage/')
+# def showAllStorage():
+#
+#
+#
+#
+# @app.route('/part/GPU/')
+# def showAllGPUs
+#
+#
+#
+#
+# @app.route('/part/powersupply/')
+# def showAllPowerSupplies():
+#
+#
+#
+#
+# @app.route('/part/soundcard')
+# def showAllSoundcards():
 
 
 
@@ -168,7 +323,7 @@ def deleteBuild(build_name):
 
 
 
-    
+
 @app.route('/wishlist/')
 def showUserWishlist():
 
@@ -179,3 +334,10 @@ def showUserWishlist():
 
 ##############################################################################
 # JSON METHODS & Stuff idk
+
+############################################################################
+
+if __name__ == '__main__':
+    app.secret_key = 'ultra_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5001)
